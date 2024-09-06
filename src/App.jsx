e335c1119f95a8be587cfe6c6700f691d0f3dca1
chandra sekhar <sekhsrchandra86@gmail.com>
2024-09-06 19:34:30 +0530

    code update

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Board from './tic_tac_game/Board'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <Board/>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
